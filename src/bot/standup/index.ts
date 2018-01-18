373772b283fd9a8c70fa887f7020a599f787755a
Carl Winkler <carl.winkler@wanews.com.au>
2018-01-18 11:04:55 +0800

    Remove debug print statement

--- src/bot/standup/index.ts
+++ src/bot/standup/index.ts
@@ -121,7 +121,6 @@ function shouldStandup(standupDays: number[], date: Date, debug: boolean) {
 
   const isStandupDay = standupDays.includes(getNow().getDay())
   const isStandupTime = getNow() > date
-  console.log({ isStandupDay, isStandupTime })
   return isStandupDay && isStandupTime
 }
 
