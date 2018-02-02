eb8f985ea25d5e5b702abb2297b63d8a8263f4df
Bennett Perkins <bennettperkins@gmail.com>
2018-02-02 15:48:12 +0800

    update incident help message

--- src/bot/commands/incident-help.ts
+++ src/bot/commands/incident-help.ts
@@ -1,4 +1,5 @@
 import { register } from '../command'
+import { sleep } from '../standup/util'
 
 register('incident-help', `View this message`, async (bot, message, config, params) => {
   
@@ -19,13 +20,40 @@ Also remember that human intuition and risk assessment are total bullshit under 
 
 In hindsight, every incident has a very simple solution.
 
-https://confluence.swmdigital.io/display/DPT/Incident+response+process
-
 `
 
+  const gettingStartedMessage = `
+
+*First Responder*
+
+The rules of engagement can be found here:
+
+https://confluence.swmdigital.io/display/DPT/Incident+response+process
+
+If you are the first to notice something is wrong, you should follow the first responder document:
+
+https://confluence.swmdigital.io/display/DPT/First+responder
+
+*Raise an incident*:
+
+\`\`\`
+@fark raise incident name
+\`\`\`
+
+This will create a jira ticket and confluence doc.
+
+`
   bot.postMessage({
     channel: message.channel,
     text: helpMessage,
     ...config.defaultParams
   })
+
+  await sleep(4)
+
+  bot.postMessage({
+    channel: message.channel,
+    text: gettingStartedMessage,
+    ...config.defaultParams
+  })  
 })
