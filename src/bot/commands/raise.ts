f49d10a699aac53dfe3c432ea2b73c61fe894480
Ryan Armstrong <ryan@cavaliercoder.com>
2018-02-02 16:29:33 +0800

    Added super-robust state management

--- src/bot/commands/raise.ts
+++ src/bot/commands/raise.ts
@@ -5,6 +5,8 @@ import { Buffer } from 'buffer'
 
 const roles: any = {}
 
+let jiraRef: string
+
 register('assign', 'Assign a role', async (bot, message, config, params) => {
   const assignee = params[0]
   const role = params.slice(-2).join(' ')
@@ -12,13 +14,13 @@ register('assign', 'Assign a role', async (bot, message, config, params) => {
 
   await bot.postMessage({
     channel: message.channel,
-    text: `${assignee} has been assigned to the ${role} role`,
+    text: `[${jiraRef ? jiraRef : 'OPS-???'}] ${assignee} has been assigned to the ${role} role`,
     ...config.defaultParams
   })
 })
 
 register('roles', 'List assigned roles', async (bot, message, config, params) => {
-  let msg = `The following roles are assigned:\n`
+  let msg = `[${jiraRef ? jiraRef : 'OPS-???'}] The following roles are assigned:\n`
   for (var role in roles) {
     msg = msg + `${role}: ${roles[role]}\n`
   }
@@ -74,6 +76,7 @@ register('raise', `Raise an incident record in JIRA`, async (bot, message, confi
     return
   }
   const ticket: any = await res.json()
+  jiraRef = ticket.key
 
   /**
    * Raise Confluence ticket
