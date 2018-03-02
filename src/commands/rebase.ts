89a82dfddc36d1c022bf6ce20c4d2fad3d9b063f
Carl Winkler <carl.winkler@wanews.com.au>
2018-03-02 16:30:44 +0800

    Fix rebase increment and alert message

--- src/commands/rebase.ts
+++ src/commands/rebase.ts
@@ -1,14 +1,14 @@
 import { register, setConfig } from '../config'
 
 register('rebase', `Tell us all you rebased! \\o/`, async (bot, msg, cfg) => {
-  await setConfig('rebaseCount', ++cfg.rebaseCount)
+  const nextCount = ++cfg.rebaseCount
+  await setConfig('rebaseCount', nextCount)
   const alarm = `:rotating_light: :rotating_light: :rotating_light:`
 
   const user = bot.users.find(user => user.id === msg.user)
   await bot.postMessage({
     channel: cfg.rebaseChannel,
-    text: `${alarm} ${user!.real_name} has rebased!\n*Rebases today*: ${cfg.rebaseCount +
-      1} ${alarm}`,
+    text: `${alarm} ${user!.real_name} has rebased! ${alaram}\n*Rebases today*: ${nextCount}`,
     ...cfg.defaultParams
   })
 })
