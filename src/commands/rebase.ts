4a162219a7c7169e2ae43b0d76cb8208a65a0863
Carl Winkler <carl.winkler@wanews.com.au>
2018-03-02 16:31:38 +0800

    Fix typo

--- src/commands/rebase.ts
+++ src/commands/rebase.ts
@@ -8,7 +8,7 @@ register('rebase', `Tell us all you rebased! \\o/`, async (bot, msg, cfg) => {
   const user = bot.users.find(user => user.id === msg.user)
   await bot.postMessage({
     channel: cfg.rebaseChannel,
-    text: `${alarm} ${user!.real_name} has rebased! ${alaram}\n*Rebases today*: ${nextCount}`,
+    text: `${alarm} ${user!.real_name} has rebased! ${alarm}\n*Rebases today*: ${nextCount}`,
     ...cfg.defaultParams
   })
 })
